6fadd5cf2bac2ba449ab15c01cf2cd2f25b80610
Your Name <you@example.com>
2021-09-10 16:02:46 +0800

    'createproject'

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+#调用日志库
+import logging
+
+# 差古剑日志记录器
+logger = logging.getLogger('django')
+
+def log(reqiest):
+    # 输出日志
+    logger.info('info')
+    return HttpResponse('test')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',log),
 ]
